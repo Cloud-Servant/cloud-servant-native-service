6217f64d4d3a989b25125b1f9d7d6fa497b74d03
Madhu5A3 <madhuv299@gmail.com>
2020-06-06 17:10:53 +0530

    Update: Application Main Stack Updated

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import AppDrawer from './components/CloudServantStackDrawer';
+
+function App(){
+  return (
+    <View>
+      <AppDrawer />
+    </View>
+  );
+}
+export default App;
