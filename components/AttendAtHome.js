540cc2d7e2f5624921c4b9a4dcb61db82bc7bf19
mmkvdev <madhuv299@gmail.com>
2020-08-15 22:18:26 +0530

    Update: Attend At Home Screen

--- components/AttendAtHome.js
+++ components/AttendAtHome.js
@@ -1,29 +1,62 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, Image } from 'react-native';
 import UploadCard from './UploadCard';
 
 const AttendAtHome = ({ navigation }) => {
     return (
-        <View style={styles.HomeContainer}>
+        /*<View style={styles.HomeContainer}>
             <UploadCard style={styles.textContainer}>
                 <Text>Your Agent will be Arriving Soon!!</Text>
             </UploadCard>
             <Button title="BACK" onPress={() => navigation.navigate('PlaceOrderScreen')}></Button>
+            https://i.pinimg.com/originals/51/ce/e6/51cee67564790d275cac21c4b754d8bb.gif
+        </View>*/
+        <View style={styles.attendAtHomeContainer}>
+            <Image 
+                source={{
+                    uri: 'https://cdn.dribbble.com/users/1226927/screenshots/3934449/ph_dribbble.gif'
+                }}
+                style={{
+                    width: 100,
+                    height: 150
+                }}
+            />
+            <View style={styles.messageContainer}>
+                <Text style={styles.textContainer}>Agent will be at
+                    <Text style={styles.innerTextContainer}> Your</Text>.
+                    Service in no time.
+                </Text>
+            </View>
+
+            <Button></Button>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    HomeContainer: {
+    attendAtHomeContainer: {
+        flex: 1,
+        paddingTop: 40,
+        padding: 2,
         alignItems: 'center',
-        marginTop: 140,
-        justifyContent: 'center'
+        backgroundColor: 'white'
+    },
+    messageContainer: {
+        textAlign: 'center',
+        margin: 20,
+        marginTop: 30
+    },
+    innerTextContainer: {
+        color: 'red'
     },
     textContainer: {
-        fontSize: 300,
-        margin: 50,
-        borderBottomColor: 'grey',
-        borderColor: 'black'
+        textAlign: 'center',
+        shadowRadius: 7,
+        backgroundColor: '#baaca6',
+        elevation: 50,
+        padding: 30,
+        fontSize: 20,
+        fontFamily: 'sans-serif-light',
     }
 });
 
